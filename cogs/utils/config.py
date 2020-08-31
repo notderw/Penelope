6a75a50fede10bcb094274a59127678581e48ef2
derw <misc.dmacht777@gmail.com>
2020-08-30 20:15:04 -0600

    utils.config implement Message

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -1,7 +1,10 @@
 import inspect
 
 from abc import ABC, abstractmethod
-from typing import get_type_hints, cast, Dict, Any, NoReturn
+from collections.abc import Iterable
+
+import typing
+from typing import Dict, Any, NoReturn
 
 import discord
 from discord.ext.commands import Bot, Context, BadArgument
@@ -17,7 +20,7 @@ class CogConfig(ABC):
 
     def __new__(cls):
         self = super().__new__(cls)
-        self.type_hints = get_type_hints(cls)
+        self.type_hints = typing.get_type_hints(cls)
         del self.type_hints['_bot'] # ehh
         return self
 
@@ -41,6 +44,8 @@ class CogConfig(ABC):
                 return self.guild.get_role(param_id)
 
             elif hint is discord.Message:
+                if not param_id:
+                    return None
                 param = list(map(int, param_id.split(':')))
                 return self._bot.get_channel(param[0]).fetch_message(param[1])
 
@@ -55,23 +60,57 @@ class CogConfig(ABC):
         return e
 
 
-    async def handle_command(self, ctx: Context, param, argument):
-        if not param:
+    async def handle_command(self, ctx: Context, *args):
+        args = list(args)
+
+        if not any(args):
             await self._send_params(ctx)
 
         else:
+            param = args.pop(0)
             try:
-                arg = await self._convert_argument(ctx, argument, param)
+                arg = await self._convert_argument(ctx, args, param)
 
                 await self._update_config(param, arg)
 
-                e = self._embed
-                e.description = f'Set **{param}** = {getattr(self, param)}'
-                await ctx.send(embed=e)
+                await ctx.send(embed=await self._single_param_embed(param))
 
             except BadArgument as e:
                 await ctx.send(e)
 
+    async def _single_param_embed(self, param):
+        e = self._embed
+        e.description = await self._render_param(param)
+        return e
+
+    async def _render_hint(self, param) -> str:
+        hint = self.type_hints[param]
+        origin = typing.get_origin(hint)
+        if origin and issubclass(origin, Iterable):
+            return f'**{param}** ({origin.__name__.capitalize()}[{typing.get_args(hint)[0].__name__}]):\n'
+
+        else:
+            return f'**{param}** ({hint.__name__}) = '
+
+    async def _render_arg(self, param) -> str:
+        arg = getattr(self, param)
+
+        if inspect.iscoroutine(arg):
+            arg = await arg
+
+        if isinstance(arg, Iterable):
+            return ''.join([f'- {s}\n' for s in arg])
+        elif isinstance(arg,  (discord.abc.Messageable, discord.Role)):
+            return f'{arg.mention}'
+        elif isinstance(arg, discord.Message):
+            return f'[Message]({arg.jump_url})'
+        else:
+            return f'{arg}'
+
+    async def _render_param(self, param):
+        return await self._render_hint(param) \
+            + await self._render_arg(param) \
+            + '\n'
 
     async def _send_params(self, ctx):
         e = self._embed
@@ -79,25 +118,27 @@ class CogConfig(ABC):
         e.description = ''
 
         for param, hint in self.type_hints.items():
-            e.description += f'**{param}** ({hint.__name__}) = '
-
-            arg = getattr(self, param)
-            if isinstance(arg,  (discord.abc.Messageable, discord.Role)):
-                e.description += f'{arg.mention}'
-            else:
-                e.description += f'{arg}'
-
-            e.description += '\n'
+            e.description += await self._render_param(param)
 
         e.description += ''
         await ctx.send(embed=e)
 
 
-    async def _convert_argument(self, ctx, argument, param) -> Any:
+    async def _convert_argument(self, ctx, args, param) -> Any:
         if param not in self.type_hints:
             raise BadArgument(f'`{param}` is not a valid config option')
 
-        return await ctx.command._actual_conversion(ctx, self.type_hints[param], argument, param)
+        hint = self.type_hints[param]
+
+        if hint is discord.Message:
+            hint = discord.TextChannel
+
+        converted = await ctx.command._actual_conversion(ctx, hint, args[0], param)
+
+        if self.type_hints[param] is discord.Message:
+            return await converted.fetch_message(args[1])
+
+        return converted
 
 
     async def _update_config(self, param, arg) -> NoReturn:
@@ -105,6 +146,9 @@ class CogConfig(ABC):
 
         if isinstance(arg, (discord.abc.Messageable, discord.Role)):
             data = {f'{self.name}.{param}_id': arg.id}
+
+        elif isinstance(arg, discord.Message):
+            data = {f'{self.name}.{param}_id': f'{arg.channel.id}:{arg.id}'}
 
         doc = await self._bot.db.guild_config.find_one_and_update(
             {"id": self._guild_id},
